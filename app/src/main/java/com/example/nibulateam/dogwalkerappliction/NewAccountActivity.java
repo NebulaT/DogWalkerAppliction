100ee8cb4b5a2163f9f44090c4da8be4dd1d0147
NebulaT <37473913+NebulaT@users.noreply.github.com>
2018-07-30 19:48:54 +0300

    upd1- יש להזיז לאמצע

--- app/src/main/java/com/example/nibulateam/dogwalkerappliction/NewAccountActivity.java
+++ app/src/main/java/com/example/nibulateam/dogwalkerappliction/NewAccountActivity.java
@@ -1,4 +1,15 @@
 package com.example.nibulateam.dogwalkerappliction;
 
-public class NewAccountActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newaccountactivity);
+    }
+
 }

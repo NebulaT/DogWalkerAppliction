4236e561a699f4ea55016ad5af181b03c9271de9
yaron Developer <nebulateam01@gmail.com>
2018-08-14 17:15:11 +0300

    yaron

--- app/src/main/java/com/example/nibulateam/dogwalkerappliction/DogWalker_RegisterActivity.java
+++ app/src/main/java/com/example/nibulateam/dogwalkerappliction/DogWalker_RegisterActivity.java
@@ -9,8 +9,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -20,8 +25,23 @@ public class DogWalker_RegisterActivity extends AppCompatActivity {
 
 
     private static final String TAG_DATE="Dog_Walker_register";
-    private Button SelectDateButton;
+    private Button SelectDateButton,DoneButton;
     private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private CheckBox SmallD,MediumD,LargeD;
+    private int year,month,day;
+
+
+    private RadioGroup radioGroupFM;
+    private RadioButton MaleRB,FemaleRB;
+
+    private Boolean isMale,isFemale;
+    private String dogSize;
+    private String Exp=null;
+    private EditText ExprianceET;
+
+    private boolean isInputGender,isInputBirthDay,isInputDogSize;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,8 +49,16 @@ public class DogWalker_RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register_dogwalker);
 
     SelectDateButton=(Button)findViewById(R.id.SelectDatebutton);
+        SmallD=(CheckBox)findViewById(R.id.SmallCheckBox);
+        MediumD=(CheckBox)findViewById(R.id.MediumCheckBox);
+        LargeD=(CheckBox)findViewById(R.id.LacheckBox);
+        MaleRB=(RadioButton)findViewById(R.id.MaleRadioButton);
+        FemaleRB=(RadioButton)findViewById(R.id.FemaleRadioButton);
+        ExprianceET=(EditText)findViewById(R.id.ExpEditText);
+        DoneButton=(Button)findViewById(R.id.Donebutton);
 
-    SelectDateButton.setOnClickListener(new View.OnClickListener() {
+
+        SelectDateButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
 
@@ -41,24 +69,175 @@ public class DogWalker_RegisterActivity extends AppCompatActivity {
         @Override
         public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
             Log.d(TAG_DATE,"onDateSet :"+i+"/"+i1+"/"+i2);
+            isInputBirthDay=true;
         }
     };
+/*
+    radioGroupFM=(RadioGroup)findViewById(R.id.MaleFemalGruopB);
+
+    radioGroupFM.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, int checkId) {
+
+            if(checkId==R.id.MaleRadioButton)
+            {
+
+            }
+            if(checkId==R.id.FemaleRadioButton)
+            {
+
+            }
+
+        }
+    });
+*/
+
+
+        MaleRB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(FemaleRB.isChecked())
+                {
+                    FemaleRB.setChecked(false);
+                    isMale=true;
+                    isFemale=false;
+                }
+            }
+        });
+
+
+
+        FemaleRB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(FemaleRB.isChecked())
+                {
+                    FemaleRB.setChecked(false);
+                    isFemale=true;
+                    isMale=false;
+                }
+            }
+        });
+
+        SmallD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(SmallD.isChecked())
+                {
+                  setDogSize("Small");
+                }
+                if(!SmallD.isChecked())
+                {
+                    RemoveDogSize("Small");
+                }
+            }
+        });
+
+        MediumD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(MediumD.isChecked())
+                {
+                    setDogSize("Medium");
+                }
+                if(!MediumD.isChecked())
+                {
+                    RemoveDogSize("Medium");
+                }
+            }
+        });
+
+        LargeD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(LargeD.isChecked())
+                {
+                    setDogSize("Large");
+                }
+                if(!LargeD.isChecked())
+                {
+                    RemoveDogSize("Large");
+                }
+            }
+        });
+
+
+        Exp=ExprianceET.getText().toString();
+
+        DoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkInput()==true)
+                {
+
+                }
+            }
+        });
 
 
     }
 
 
+
     private void ChooseBirthday()
     {
         Calendar calendar=Calendar.getInstance();
-        int year=calendar.get(Calendar.YEAR);
-        int month=calendar.get(Calendar.MONTH);
-        int day=calendar.get(Calendar.DAY_OF_MONTH);
+        year=calendar.get(Calendar.YEAR);
+         month=calendar.get(Calendar.MONTH);
+         day=calendar.get(Calendar.DAY_OF_MONTH);
 
 
         DatePickerDialog datePickerDialog=new DatePickerDialog(DogWalker_RegisterActivity.this,android.R.style.Theme_Holo_Light_Dialog_MinWidth,mDateSetListener,year,month,day);
 
         datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         datePickerDialog.show();
+    }
+
+    private void setDogSize(String size)
+    {
+        if(dogSize!=null)
+        {
+            dogSize+=size;
+        }
+        else
+        {
+            dogSize=size;
+        }
+    }
+    private void RemoveDogSize(String size)
+    {
+        if(dogSize.contains(size))
+        {
+            dogSize.replace(size,"");
+        }
+    }
+
+    private boolean checkInput()
+    {
+
+        if(isMale==true || isFemale==true)
+        {
+            isInputGender=true;
+        }
+        else
+        {
+            Toast.makeText(DogWalker_RegisterActivity.this,"Please choose gender",Toast.LENGTH_SHORT).show();
+        }
+        if(SmallD.isChecked() || LargeD.isChecked() || MediumD.isChecked())
+        {
+            isInputDogSize=true;
+        }
+        else
+        {
+            Toast.makeText(DogWalker_RegisterActivity.this,"Please choose dog type",Toast.LENGTH_SHORT).show();
+
+        }
+        if(isInputBirthDay==true && isInputDogSize==true && isInputGender==true)
+        {
+            return true;
+        }
+
+        return false;
+
     }
 }

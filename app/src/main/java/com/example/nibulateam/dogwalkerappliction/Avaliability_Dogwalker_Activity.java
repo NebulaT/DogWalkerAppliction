8601cc094c7030faa7db9aa2ac4a1926a09810eb
yaron Developer <nebulateam01@gmail.com>
2018-08-19 13:05:27 +0300

    yaron

--- app/src/main/java/com/example/nibulateam/dogwalkerappliction/Avaliability_Dogwalker_Activity.java
+++ app/src/main/java/com/example/nibulateam/dogwalkerappliction/Avaliability_Dogwalker_Activity.java
@@ -4,9 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
 
 public class Avaliability_Dogwalker_Activity extends Activity {
 
+    private static final int ROW = 7;
+    private static final int COL = 5;
+
+    private TableLayout tableLayout;
+    private ImageButton[][] imageButtons;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -14,12 +22,29 @@ public class Avaliability_Dogwalker_Activity extends Activity {
 
         setContentView(R.layout.activity_avaliability_dogwalker);
 
-        DisplayMetrics dm=new DisplayMetrics();
+        DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
-        int width=dm.widthPixels;
-        int height=dm.heightPixels;
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width*.8),(int)(height*.3));
+        getWindow().setLayout((int) (width * .8), (int) (height * .9));
+
+
+        imageButtons = new ImageButton[ROW][COL];
+
+
+        for (int i=0;i<ROW;i++)
+        {
+            for(int j=0 ;j<COL;j++)
+            {
+                String temp="ava"+i+j;
+               int Res=getResources().getIdentifier(temp,"id",getPackageName());
+                imageButtons[i][j]=(ImageButton)findViewById(Res);
+
+            //    imageButtons[i][j].setOnClickListener(this);
+            }
+        }
+
     }
 }
